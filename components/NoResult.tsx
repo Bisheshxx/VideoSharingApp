ccefcc445a78da61c68e4187925d5dd0b5577097
User <User@DESKTOP-SOI26OL>
2022-07-21 11:19:44 +0545

    first commit

---
+++ components/NoResult.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+interface IProps {
+  text: string;
+}
+const NoResult = ({ text }: IProps) => {
+  return <div></div>;
+};
+
+export default NoResult;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSr, setIsSSr] = useState(true);
+  useEffect(() => {
+    setIsSSr(false);
+  }, []);
+  if (isSSr) return null;
+  return (
+    <GoogleOAuthProvider
+      clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}
+    >
+      <Navbar />
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          <Sidebar />
+        </div>
+        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </GoogleOAuthProvider>
+  );
+};
 
-export default MyApp
+export default MyApp;
